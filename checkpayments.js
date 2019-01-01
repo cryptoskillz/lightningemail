1be6fe68aa68015c82111c7c588cbe5c56c2c0b4
cryptoskillz <cryptoskillz@protonmail.com>
2019-01-02 01:41:20 +0700

    clean up

--- checkpayments.js
+++ checkpayments.js
@@ -1,13 +1,8 @@
-//TODO
 //check the process env vars
 require('dotenv').config();
 // Load the SDK for JavaScript
 var AWS = require('aws-sdk');
-var emlformat = require('eml-format');
 const request = require('request');
-var Promise = require('bluebird');
-
-let keyid = '';
 AWS.config = new AWS.Config();
 AWS.config.accessKeyId = process.env.ACCESSKEY;
 AWS.config.secretAccessKey = process.env.SECRETACCESSKEY;
@@ -15,10 +10,12 @@ AWS.config.region = "eu-west-1";
 // Create an SQS service object
 var s3 = new AWS.S3();
 
+//set up the bucket with the ch_ as a filter.
 var params = {
-  Bucket:  process.env.BUCKET, /* required */
-  Prefix: 'ch_'  // Can be your folder name
+  Bucket:  process.env.BUCKET, 
+  Prefix: 'ch_' 
 };
+//get all the objects that match
 s3.listObjectsV2(params, function(err, data) 
 {
   if (err) console.log(err, err.stack); // an error occurred
@@ -27,9 +24,9 @@ s3.listObjectsV2(params, function(err, data)
   	//loop through the payments
   	data.Contents.forEach(function(obj,index)
   	{
-		console.log('Checking '+obj.Key)
+		console.log('Checking '+obj.Key);
+		//build the charge reqiests
 		var request = require("request");
-
 		var endpoint = process.env.STRIKEENDPOINT;
 		var api_key = process.env.STRIKEAPIKEY;
 		var charge_id = obj.Key;
@@ -46,10 +43,11 @@ s3.listObjectsV2(params, function(err, data)
 		    pass: '',
 		  }
 		};
-
+		//call it
 		request(options, function (error, response, body) {
 		  if (error) throw new Error(error);
 		  console.log('Paid'+body.paid);
+		  //check if it was paid
 		  if (body.paid == true)
 		  {
 		  	//it has been paid so we can do somethign with the email forward it or something. 
